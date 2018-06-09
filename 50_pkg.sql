c3b46f70ff96ecbf214f552b0bbdf95cac0ff880
unleo <dselyanin@gmail.com>
2018-06-09 13:01:07 +0300

    issue #20, изменение в pkg_op_after: при операции create, производится запись в poma.pkg

--- 50_pkg.sql
+++ 50_pkg.sql
@@ -259,7 +259,7 @@ $_$
     r_pkg := poma.pkg(a_code);
     CASE a_op
       WHEN 'create' THEN
-        IF a_code = 'poma' AND a_schema = 'poma' THEN
+        IF r_pkg IS NULL THEN
           INSERT INTO poma.pkg (id, code, schemas, log_name, user_name, ssh_client, op) VALUES 
             (NEXTVAL('poma.pkg_id_seq'), a_code, ARRAY[a_schema], a_log_name, a_user_name, a_ssh_client, a_op)
             RETURNING * INTO r_pkg
