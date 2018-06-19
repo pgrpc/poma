bf1826dbc25a1358e6f737d71b780a5d0f85b3e4
unleo <dselyanin@gmail.com>
2018-06-19 15:27:46 +0300

    issue #28, добавлен тест comment_function

--- 90_comment.md
+++ 90_comment.md
@@ -2,6 +2,9 @@
 ## poma/90_comment
 
 ```sql
+/*
+  Test comment schema
+*/
 SELECT (CASE WHEN (select obj_description(to_regnamespace('poma'))) = 'Postgresql projects Makefile' THEN TRUE ELSE FALSE END) AS is_set_comment
 ;
 ```
@@ -9,3 +12,32 @@ SELECT (CASE WHEN (select obj_description(to_regnamespace('poma'))) = 'Postgresq
 |---------------
 |t
 
+## poma/90_comment
+
+```sql
+/*
+  Test comment function
+*/
+SELECT p.proname
+, pg_catalog.pg_get_function_identity_arguments(p.oid)
+, obj_description(p.oid, 'pg_proc')
+  FROM pg_catalog.pg_proc p
+ WHERE p.proname IN ('comment','test_arg')
+;
+```
+|proname  |                    pg_get_function_identity_arguments                    | obj_description 
+|---------|--------------------------------------------------------------------------|-----------------
+|comment  | a_type character, a_code name, a_comment text, VARIADIC a_columns text[] | te'st
+|test_arg |                                                                          | all test_arg
+|test_arg | a text                                                                   | all test_arg
+
+## poma/90_comment
+
+```sql
+/*
+  Тест comment table
+*/
+
+
+;
+```
--- 90_comment.sql
+++ 90_comment.sql
@@ -4,16 +4,77 @@
 
 -- ----------------------------------------------------------------------------
 SELECT poma.test('comment_schema'); -- BOT
-/*
-  Тест comment schema
-*/
 SELECT poma.comment('n','poma','Postgresql projects Makefile');
+/*
+  Test comment schema
+*/
 SELECT (CASE WHEN (select obj_description(to_regnamespace('poma'))) = 'Postgresql projects Makefile' THEN TRUE ELSE FALSE END) AS is_set_comment;  -- EOT
 -- ----------------------------------------------------------------------------
 
+-- ----------------------------------------------------------------------------
+SELECT poma.test('comment_function'); -- BOT
+\set QUIET on
+create or replace function poma.test_arg() returns void language sql as
+$_$ 
+ SET CLIENT_MIN_MESSAGES = 'DEBUG';
+$_$;
+create or replace function poma.test_arg(a TEXT) returns void language sql as
+$_$ 
+ SET CLIENT_MIN_MESSAGES = a; --'INFO';
+$_$;
+\set QUIET off
 
+-- вызов коментирования функций
+SELECT poma.comment('f','poma.comment',E'te''st');
+SELECT poma.comment('f','poma.test_arg','all test_arg');
 
+/*
+  Test comment function
+*/
+SELECT p.proname
+, pg_catalog.pg_get_function_identity_arguments(p.oid)
+, obj_description(p.oid, 'pg_proc')
+  FROM pg_catalog.pg_proc p
+ WHERE p.proname IN ('comment','test_arg')
+; -- EOT
+-- ----------------------------------------------------------------------------
 
+-- ----------------------------------------------------------------------------
+--SELECT poma.test('comment_table'); -- BOT
+/*
+  Тест comment table
+*/
+--SELECT poma.comment('t','poma.pkg','{id,"идентификатор","code","код",schemas,"наименование схемы"}');
+--SELECT (CASE WHEN (select obj_description(to_regnamespace('poma'))) = 'Postgresql projects Makefile' THEN TRUE ELSE FALSE END) AS is_set_comment;  -- EOT
+-- ----------------------------------------------------------------------------
+
+/*
+id integer NOT NULL,
+  code text NOT NULL,
+  schemas name[],
+  op poma.t_pkg_op,
+  version numeric NOT NULL DEFAULT 0,
+  log_name text,
+  user_name text,
+  ssh_client text,
+  usr text DEFAULT "current_user"(),
+  ip inet DEFAULT inet_client_addr(),
+  stamp timestamp without time zone DEFAULT now(),
+select nspname, relname, attname, format_type(atttypid, atttypmod), obj_description(c.oid), col_description(c.oid, a.attnum) 
+from pg_class c join pg_attribute a on (a.attrelid = c.oid) join pg_namespace n on (n.oid = c.relnamespace)
+where nspname='poma' and relname='pkg';
+*/
+/*
+      WHEN a_type = 'n' THEN 'SCHEMA'
+      WHEN a_type = 't' THEN 'TABLE'
+      WHEN a_type = 'v' THEN 'VIEW'
+      WHEN a_type = 'c' THEN 'COLUMN'
+      WHEN a_type = 'T' THEN 'TYPE'
+      WHEN a_type = 'D' THEN 'DOMAIN'
+      WHEN a_type = 'f' THEN 'FUNCTION'
+      WHEN a_type = 's' THEN 'SEQUENCE'
+
+*/
 /*
 -- type
 set local search_path = poma,public;
@@ -31,24 +92,5 @@ SELECT comment('n',NULL,'current=ok');
 SELECT comment('n','rpc','rpc=ok');
 \dn+
 
-create or replace function rpc.test_arg() returns void language sql as
-$_$ 
- SET CLIENT_MIN_MESSAGES = 'DEBUG';
-$_$;
-create or replace function rpc.test_arg(a TEXT) returns void language sql as
-$_$ 
- SET CLIENT_MIN_MESSAGES = a; --'INFO';
-$_$;
-
--- func
-SELECT poma.comment('f','poma.comment',E'te''st');
-SELECT poma.comment('f','rpc.test_arg','all test_arg');
-
-SELECT p.proname
-, pg_catalog.pg_get_function_identity_arguments(p.oid)
-, obj_description(p.oid, 'pg_proc')
-  FROM pg_catalog.pg_proc p
- WHERE p.proname IN ('comment','test_arg')
-;
 
 */
